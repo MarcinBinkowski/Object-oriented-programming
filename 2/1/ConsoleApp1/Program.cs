881de01b649df1e8eb3492bb89fe6ada2f441287
Marcin <marcin.binkowski@onet.eu>
2019-03-05 21:48:43 +0100

    List 2 ex 1

---
+++ 2/1/ConsoleApp1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            RandomWordStream newRandomWordStream = new RandomWordStream();
+            for (int i = 0; i < 12; i++)
+                Console.WriteLine(newRandomWordStream.Next());
+            Console.ReadLine();
+        }
+    }
+    public class IntStream
+    {
+        protected int nextNumber = -1;
+
+        public virtual int Next()
+        {
+            if (Eos() == true)
+            {
+                return -1;
+            }
+            nextNumber++;
+            return nextNumber;
+        }
+
+        public bool Eos()
+        {
+            if (nextNumber >= int.MaxValue)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            nextNumber = -1;
+        }
+    }
+
+    public class PrimeStream : IntStream
+    {
+        private bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if ((number % i) == 0)
+                    return false;
+                }
+            return true;
+        }
+
+        public override int Next()
+        {
+            nextNumber++;
+            while (IsPrime(nextNumber) == false)
+                nextNumber++;
+            if (Eos() == true)
+                return -1;
+            else 
+                return nextNumber;
+        }
+    }
+
+    public class RandomStream : IntStream
+    {
+        Random randomNumber = new Random();
+
+        public override int Next()
+        {
+            return randomNumber.Next();
+        }
+    }
+
+    public class RandomWordStream : PrimeStream
+    {
+        private static string chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";
+        RandomStream randInt = new RandomStream();
+
+        public new string Next()
+        {
+            string text = "";
+            base.Next();
+            int temp = nextNumber;
+            while (temp > 0)
+            {
+                int newRand = randInt.Next();
+                text = text + chars[newRand % chars.Length];
+                temp--;
+            }
+            return text;
+        }
+    }
+}
